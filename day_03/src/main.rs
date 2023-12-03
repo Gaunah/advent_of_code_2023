2145ec13eaf3faea894b94b746722147eedba956
gaunah <christoph@zelinski.de>
2023-12-03 18:36:03 +0100

    refactoring for day03

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,5 +1,4 @@
 use regex::Regex;
-use std::cmp;
 
 const PLACEHOLDER: char = '.';
 
@@ -9,15 +8,18 @@ fn main() {
     println!("Answer part2: {}", part2(input));
 }
 
-fn contains_symbol(slice: &str) -> bool {
-    slice
-        .find(|c: char| !c.is_digit(10) && c != PLACEHOLDER)
-        .is_some()
+fn part1(input: &str) -> u32 {
+    process_input_window(input, sum_part_numbers)
 }
 
-fn part1(input: &str) -> u32 {
-    let re = Regex::new(r"(\d+)").expect("Should be valid regex!");
+fn part2(input: &str) -> u32 {
+    process_input_window(input, sum_gear_ratios)
+}
 
+fn process_input_window<F>(input: &str, process_window_fn: F) -> u32
+where
+    F: Fn([&str; 3], usize) -> u32,
+{
     let line_len = input.lines().next().expect("Should not be empty!").len();
     let dots = PLACEHOLDER.to_string().repeat(line_len);
     std::iter::once(dots.as_str())
@@ -27,74 +29,75 @@ fn part1(input: &str) -> u32 {
         .windows(3)
         .fold(0, |total, window| {
             if let [above, mid, below] = window {
-                total
-                    + re.captures_iter(mid)
-                        .filter_map(|cap| {
-                            let c = cap.get(0).unwrap();
-                            let start_e = c.start().saturating_sub(1);
-                            let end_e = cmp::min(c.end() + 1, line_len);
-                            if contains_symbol(&above[start_e..end_e])
-                                || contains_symbol(&mid[start_e..end_e])
-                                || contains_symbol(&below[start_e..end_e])
-                            {
-                                Some(cap[0].parse::<u32>().expect("Should be a positiv number!"))
-                            } else {
-                                None
-                            }
-                        })
-                        .sum::<u32>()
+                total + process_window_fn([above, mid, below], line_len)
             } else {
                 total
             }
         })
 }
 
-fn part2(input: &str) -> u32 {
+fn sum_part_numbers(window: [&str; 3], line_len: usize) -> u32 {
     let re = Regex::new(r"(\d+)").expect("Should be valid regex!");
+    let [above, mid, below] = window;
 
-    let line_len = input.lines().next().expect("Should not be empty!").len();
-    let dots = PLACEHOLDER.to_string().repeat(line_len);
-    std::iter::once(dots.as_str())
-        .chain(input.lines())
-        .chain(std::iter::once(dots.as_str()))
-        .collect::<Vec<&str>>()
-        .windows(3)
-        .fold(0, |total, window| {
-            if let [a, m, b] = window {
-                total
-                    + m.match_indices("*")
-                        .filter_map(|(i, _)| {
-                            let gear_numbers: Vec<u32> = re
-                                .captures_iter(a)
-                                .chain(re.captures_iter(m))
-                                .chain(re.captures_iter(b))
-                                .map(|cap| cap.get(0).unwrap())
-                                .filter_map(|number| {
-                                    let start_e = number.start().saturating_sub(1);
-                                    let end_e = cmp::min(number.end() + 1, line_len);
-                                    if (start_e..end_e).contains(&i) {
-                                        Some(
-                                            number
-                                                .as_str()
-                                                .parse()
-                                                .expect("Should be a positiv number!"),
-                                        )
-                                    } else {
-                                        None
-                                    }
-                                })
-                                .collect();
-                            if gear_numbers.len() == 2 {
-                                Some(gear_numbers.first().unwrap() * gear_numbers.last().unwrap())
-                            } else {
-                                None
-                            }
-                        })
-                        .sum::<u32>()
+    re.captures_iter(mid)
+        .filter_map(|cap| {
+            let number = cap.get(0).unwrap();
+            let start = number.start().saturating_sub(1);
+            let end = number.end().saturating_add(1);
+            let clamped_range = start..end.clamp(0, line_len);
+            if contains_symbol(&above[clamped_range.clone()])
+                || contains_symbol(&mid[clamped_range.clone()])
+                || contains_symbol(&below[clamped_range])
+            {
+                Some(cap[0].parse::<u32>().expect("Should be a positiv number!"))
             } else {
-                total
+                None
             }
         })
+        .sum()
+}
+
+fn contains_symbol(slice: &str) -> bool {
+    slice
+        .find(|c: char| !c.is_ascii_digit() && c != PLACEHOLDER)
+        .is_some()
+}
+
+fn sum_gear_ratios(window: [&str; 3], line_len: usize) -> u32 {
+    let re = Regex::new(r"(\d+)").expect("Should be valid regex!");
+    let [above, mid, below] = window;
+
+    mid.match_indices('*')
+        .filter_map(|(i, _)| {
+            let gear_numbers: Vec<u32> = re
+                .captures_iter(above)
+                .chain(re.captures_iter(mid))
+                .chain(re.captures_iter(below))
+                .map(|cap| cap.get(0).unwrap())
+                .filter_map(|number| {
+                    let start = number.start().saturating_sub(1);
+                    let end = number.end().saturating_add(1);
+                    let clamped_range = start..end.clamp(0, line_len);
+                    if clamped_range.contains(&i) {
+                        Some(
+                            number
+                                .as_str()
+                                .parse()
+                                .expect("Should be a positiv number!"),
+                        )
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+            if gear_numbers.len() == 2 {
+                Some(gear_numbers.first().unwrap() * gear_numbers.last().unwrap())
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 #[cfg(test)]
