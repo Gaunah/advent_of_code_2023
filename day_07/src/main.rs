6a5bd954d368963c7b7cfcbb91acdf290f96322b
gaunah <christoph@zelinski.de>
2023-12-09 15:55:29 +0100

    Solution for day07 part 1

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -32,60 +32,60 @@ fn parse_input(input: &str) -> Vec<Hand> {
 
 fn cmp_hands(a: &Hand, b: &Hand) -> Ordering {
     match get_hand_type(a).cmp(&get_hand_type(b)) {
-        Ordering::Less => return Ordering::Less,
-        Ordering::Greater => return Ordering::Greater,
-        Ordering::Equal => {
-            let (first_card, second_card) = a
-                .cards
-                .chars()
-                .zip(b.cards.chars())
-                .find(|(first_card, second_card)| {
-                    get_card_value(first_card) != get_card_value(second_card)
-                })
-                .unwrap();
-
-            get_card_value(&first_card).cmp(&get_card_value(&second_card))
-        }
+        Ordering::Equal => a
+            .cards
+            .chars()
+            .zip(b.cards.chars())
+            .find_map(|(first_card, second_card)| {
+                let first_val = get_card_value(&first_card);
+                let second_val = get_card_value(&second_card);
+                (first_val != second_val).then(|| first_val.cmp(&second_val))
+            })
+            .unwrap_or(Ordering::Equal),
+        other => other,
     }
 }
 
 fn get_hand_type(hand: &Hand) -> u8 {
     let mut card_counts = HashMap::new();
+    card_counts.reserve(5);
     for ch in hand.cards.chars() {
         *card_counts.entry(ch).or_insert(0) += 1;
     }
+    card_counts.shrink_to_fit();
 
-    let values: Vec<_> = card_counts.values().into_iter().collect();
+    let mut values: Vec<_> = card_counts.values().into_iter().collect();
+    values.sort_unstable();
     match values.as_slice() {
         [1, 1, 1, 1, 1] => 0, // high card
-        [2, 1, 1, 1] => 1,    // one pair
-        [2, 2, 1] => 2,       // two pair
-        [3, 1, 1] => 3,       // three of a kind
-        [3, 2] => 4,          // full house
-        [4, 1] => 5,          // four of a kind
-        [5] => 5,             // five of a kind
+        [1, 1, 1, 2] => 1,    // one pair
+        [1, 2, 2] => 2,       // two pair
+        [1, 1, 3] => 3,       // three of a kind
+        [2, 3] => 4,          // full house
+        [1, 4] => 5,          // four of a kind
+        [5] => 6,             // five of a kind
         _ => panic!("There should not be any other combination!"),
     }
 }
 
-fn get_card_value(c: &char) -> u8 {
-    match c {
+fn get_card_value(card: &char) -> u8 {
+    match card {
         'T' => 10,
         'J' => 11,
         'Q' => 12,
         'K' => 13,
         'A' => 14,
-        _ => c.to_digit(10).expect("Should be one of 2..=9") as u8,
+        _ => card.to_digit(10).expect("Should be one of 2..=9") as u8,
     }
 }
 
 fn part1(input: &str) -> u32 {
     let mut hands = parse_input(input);
     hands.sort_unstable_by(|a, b| cmp_hands(a, b));
-    dbg!(&hands);
-    //hands.iter().enumerate().fold(0, |acc, pair| {})
-
-    todo!();
+    hands.iter().enumerate().fold(0, |acc, pair| {
+        let (rank, hand) = pair;
+        acc + (1 + rank as u32) * hand.bid
+    })
 }
 
 // fn part2(input: &str) -> u32 {
