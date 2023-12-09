d3381ef1fecca15e89dfd3d9ff7937b671a71b20
gaunah <christoph@zelinski.de>
2023-12-09 17:59:36 +0100

    Solution for day07 part 2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -2,8 +2,8 @@ use std::{cmp::Ordering, collections::HashMap};
 
 fn main() {
     let input = include_str!("../input.txt");
-    println!("Answer part1: {}", part1(input));
-    // println!("Answer part2: {}", part2(input));
+    println!("Answer part1: {}", get_solution(input, false));
+    println!("Answer part2: {}", get_solution(input, true));
 }
 
 #[derive(Debug)]
@@ -30,15 +30,15 @@ fn parse_input(input: &str) -> Vec<Hand> {
         .collect()
 }
 
-fn cmp_hands(a: &Hand, b: &Hand) -> Ordering {
-    match get_hand_type(a).cmp(&get_hand_type(b)) {
+fn cmp_hands(a: &Hand, b: &Hand, joker: bool) -> Ordering {
+    match get_hand_type(a, joker).cmp(&get_hand_type(b, joker)) {
         Ordering::Equal => a
             .cards
             .chars()
             .zip(b.cards.chars())
             .find_map(|(first_card, second_card)| {
-                let first_val = get_card_value(&first_card);
-                let second_val = get_card_value(&second_card);
+                let first_val = get_card_value(&first_card, joker);
+                let second_val = get_card_value(&second_card, joker);
                 (first_val != second_val).then(|| first_val.cmp(&second_val))
             })
             .unwrap_or(Ordering::Equal),
@@ -46,13 +46,25 @@ fn cmp_hands(a: &Hand, b: &Hand) -> Ordering {
     }
 }
 
-fn get_hand_type(hand: &Hand) -> u8 {
+fn process_joker(card_counts: &mut HashMap<char, u8>) {
+    let joker_count = card_counts.remove(&'J').unwrap_or(0);
+    let (&modal_card, _) = card_counts
+        .iter()
+        .max_by_key(|&(_, &val)| val)
+        .expect("Should not be empty!");
+
+    *(card_counts.get_mut(&modal_card).unwrap()) += joker_count;
+}
+
+fn get_hand_type(hand: &Hand, joker: bool) -> u8 {
     let mut card_counts = HashMap::new();
-    card_counts.reserve(5);
     for ch in hand.cards.chars() {
         *card_counts.entry(ch).or_insert(0) += 1;
     }
-    card_counts.shrink_to_fit();
+
+    if joker && card_counts.len() > 1 {
+        process_joker(&mut card_counts);
+    }
 
     let mut values: Vec<_> = card_counts.values().into_iter().collect();
     values.sort_unstable();
@@ -64,14 +76,20 @@ fn get_hand_type(hand: &Hand) -> u8 {
         [2, 3] => 4,          // full house
         [1, 4] => 5,          // four of a kind
         [5] => 6,             // five of a kind
-        _ => panic!("There should not be any other combination!"),
+        _ => panic!("Invalid hand combination!"),
     }
 }
 
-fn get_card_value(card: &char) -> u8 {
+fn get_card_value(card: &char, joker: bool) -> u8 {
     match card {
         'T' => 10,
-        'J' => 11,
+        'J' => {
+            if joker {
+                1
+            } else {
+                11
+            }
+        }
         'Q' => 12,
         'K' => 13,
         'A' => 14,
@@ -79,18 +97,14 @@ fn get_card_value(card: &char) -> u8 {
     }
 }
 
-fn part1(input: &str) -> u32 {
+fn get_solution(input: &str, joker: bool) -> u32 {
     let mut hands = parse_input(input);
-    hands.sort_unstable_by(|a, b| cmp_hands(a, b));
+    hands.sort_unstable_by(|a, b| cmp_hands(a, b, joker));
     hands.iter().enumerate().fold(0, |acc, pair| {
         let (rank, hand) = pair;
         acc + (1 + rank as u32) * hand.bid
     })
 }
-
-// fn part2(input: &str) -> u32 {
-//     0
-// }
 
 #[cfg(test)]
 mod test {
@@ -104,11 +118,11 @@ QQQJA 483";
 
     #[test]
     fn case1() {
-        assert_eq!(part1(TEST_INPUT), 6440);
+        assert_eq!(get_solution(TEST_INPUT, false), 6440);
     }
 
-    // #[test]
-    // fn case2() {
-    //     assert_eq!(part2(TEST_INPUT), 0);
-    // }
+    #[test]
+    fn case2() {
+        assert_eq!(get_solution(TEST_INPUT, true), 5905);
+    }
 }
