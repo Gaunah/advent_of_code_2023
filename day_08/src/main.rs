2fc8c03fc77cffe27226dc7d1436562d57d007be
gaunah <christoph@zelinski.de>
2023-12-10 21:26:07 +0100

    Solution for day08 part 2

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -1,4 +1,3 @@
-//use petgraph::dot::Dot;
 use petgraph::graph::Graph;
 use petgraph::visit::EdgeRef;
 use regex::Regex;
@@ -7,7 +6,7 @@ use std::collections::HashMap;
 fn main() {
     let input = include_str!("../input.txt");
     println!("Answer part1: {}", part1(input));
-    // println!("Answer part2: {}", part2(input));
+    println!("Answer part2: {}", part2(input));
 }
 
 fn parse_input(input: &str) -> (&str, Graph<String, char>) {
@@ -20,7 +19,7 @@ fn parse_input(input: &str) -> (&str, Graph<String, char>) {
     let re = Regex::new(r"(\w+) = \((\w+), (\w+)\)").unwrap();
 
     for line in lines {
-        let Some(caps) = re.captures(&line) else {
+        let Some(caps) = re.captures(line) else {
             panic!("Invalid line format!");
         };
 
@@ -47,29 +46,35 @@ fn parse_input(input: &str) -> (&str, Graph<String, char>) {
     (instructions, graph)
 }
 
-fn part1(input: &str) -> u32 {
+fn part1(input: &str) -> u64 {
     let (instructions, graph) = parse_input(input);
+    get_step_count(instructions, &graph, "AAA", |node| node == "ZZZ")
+}
 
+fn get_step_count(
+    instructions: &str,
+    graph: &Graph<String, char>,
+    start: &str,
+    end_condition: impl Fn(&str) -> bool,
+) -> u64 {
     let mut current_node = graph
         .node_indices()
-        .find(|&node| graph[node] == "AAA")
-        .expect("Graph should contain node AAA!");
+        .find(|&node| graph[node] == start)
+        .expect("Graph should contain start node!");
 
     let mut step_count = 0;
     for direction in instructions.chars().cycle() {
-        current_node = graph
+        if let Some(edge) = graph
             .edges(current_node)
-            .find_map(|edge| {
-                if edge.weight() == &direction {
-                    Some(edge.target())
-                } else {
-                    None
-                }
-            })
-            .unwrap();
+            .find(|edge| edge.weight() == &direction)
+        {
+            current_node = edge.target();
+        } else {
+            panic!("No valid edge found for direction {}", direction);
+        }
 
         step_count += 1;
-        if graph[current_node] == String::from("ZZZ") {
+        if end_condition(&graph[current_node]) {
             break;
         }
     }
@@ -77,15 +82,59 @@ fn part1(input: &str) -> u32 {
     step_count
 }
 
-// fn part2(input: &str) -> u32 {
-//     0
-// }
+fn part2(input: &str) -> u64 {
+    let (instructions, graph) = parse_input(input);
+
+    let all_start_nodes = graph
+        .node_indices()
+        .filter(|&node| graph[node].ends_with('A'));
+
+    let step_count_vec: Vec<u64> = all_start_nodes
+        .map(|node| {
+            get_step_count(instructions, &graph, &graph[node], |node| {
+                node.ends_with('Z')
+            })
+        })
+        .collect();
+
+    lcm_of_vec(&step_count_vec)
+}
+
+fn lcm(first: u64, second: u64) -> u64 {
+    first * second / gcd(first, second)
+}
+
+fn gcd(first: u64, second: u64) -> u64 {
+    let mut max = first;
+    let mut min = second;
+    if min > max {
+        std::mem::swap(&mut max, &mut min);
+    }
+
+    loop {
+        let res = max % min;
+        if res == 0 {
+            return min;
+        }
+
+        max = min;
+        min = res;
+    }
+}
+
+fn lcm_of_vec(numbers: &[u64]) -> u64 {
+    numbers
+        .iter()
+        .fold(1, |acc, &num| lcm(acc, std::cmp::max(num, 1)))
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const TEST_INPUT_1: &str = "RL
+    #[test]
+    fn case1() {
+        let input1 = "RL
 
 AAA = (BBB, CCC)
 BBB = (DDD, EEE)
@@ -94,21 +143,28 @@ DDD = (DDD, DDD)
 EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)";
+        assert_eq!(part1(input1), 2);
 
-    const TEST_INPUT_2: &str = "LLR
+        let input2 = "LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)";
-
-    #[test]
-    fn case1() {
-        assert_eq!(part1(TEST_INPUT_1), 2);
-        assert_eq!(part1(TEST_INPUT_2), 6);
+        assert_eq!(part1(input2), 6);
     }
 
-    // #[test]
-    // fn case2() {
-    //     assert_eq!(part2(TEST_INPUT), 0);
-    // }
+    #[test]
+    fn case2() {
+        let input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX) ";
+        assert_eq!(part2(input), 6);
+    }
 }
