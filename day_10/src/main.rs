1416ae4ac97a3f19c917008a5f03ea9740fbcc55
gaunah <christoph@zelinski.de>
2023-12-12 15:58:36 +0100

    Solution for day10 part 1

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -4,11 +4,37 @@ fn main() {
     // println!("Answer part2: {}", part2(input));
 }
 
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
 fn part1(input: &str) -> u32 {
     let matrix: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    let start_coord = find_start_coord(&matrix).expect("Should contain start!");
-    dbg!(start_coord);
-    0
+
+    let mut current_coord = find_start_coord(&matrix).expect("Should contain start!");
+    let mut next_direction = Some(Direction::East); // Assumed first step East is valid
+    let mut steps = 0;
+
+    // loops until we hit 'S'
+    while let Some(dir) = next_direction {
+        current_coord = update_coord(current_coord, dir);
+        next_direction = get_next_direction(&matrix, current_coord, dir);
+        steps += 1;
+    }
+    steps / 2
+}
+
+fn update_coord((row, col): (usize, usize), direction: Direction) -> (usize, usize) {
+    match direction {
+        Direction::North => (row.saturating_sub(1), col),
+        Direction::East => (row, col + 1),
+        Direction::South => (row + 1, col),
+        Direction::West => (row, col.saturating_sub(1)),
+    }
 }
 
 fn find_start_coord(matrix: &[Vec<char>]) -> Option<(usize, usize)> {
@@ -18,6 +44,35 @@ fn find_start_coord(matrix: &[Vec<char>]) -> Option<(usize, usize)> {
         }
     }
     None
+}
+
+fn get_next_direction(
+    matrix: &[Vec<char>],
+    current: (usize, usize),
+    from: Direction,
+) -> Option<Direction> {
+    let (row, col) = current;
+    match (matrix[row][col], from) {
+        ('|', Direction::North) => Some(Direction::North),
+        ('|', Direction::South) => Some(Direction::South),
+
+        ('-', Direction::East) => Some(Direction::East),
+        ('-', Direction::West) => Some(Direction::West),
+
+        ('L', Direction::South) => Some(Direction::East),
+        ('L', Direction::West) => Some(Direction::North),
+
+        ('J', Direction::South) => Some(Direction::West),
+        ('J', Direction::East) => Some(Direction::North),
+
+        ('7', Direction::North) => Some(Direction::West),
+        ('7', Direction::East) => Some(Direction::South),
+
+        ('F', Direction::North) => Some(Direction::East),
+        ('F', Direction::West) => Some(Direction::South),
+        ('S', _) => None,
+        other => panic!("Invalid combination! {:?}", other),
+    }
 }
 
 // fn part2(input: &str) -> u32 {
